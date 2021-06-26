4a59e3b855d203a038e20c9594ccd8180cecc8ff
Code Chaser <codechaser2002@gmail.com>
2021-06-27 02:20:40 +0530

    included required libraries

--- headers/driver.hpp
+++ headers/driver.hpp
@@ -1,4 +1,11 @@
 #ifndef DRIVER
 #define DRIVER
+using namespace std;
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "./person.hpp"
 
 #endif // !DRIVER
--- headers/nurse.hpp
+++ headers/nurse.hpp
@@ -1,4 +1,11 @@
 #ifndef NURSE
 #define NURSE
+using namespace std;
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "./person.hpp"
 
 #endif // !NURSE
